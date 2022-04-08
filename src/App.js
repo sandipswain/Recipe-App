3039f298402e9c3c1cce38dc18891099b0941bed
Sandip Swain <swainsandip9@gmail.com>
2022-04-08 21:00:59 +0530

    Added the Logo to move to the Home Screen

--- src/App.js
+++ src/App.js
@@ -3,10 +3,17 @@ import Search from "./components/Search";
 import Pages from "./pages/Pages";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import { GiKnifeFork } from "react-icons/gi";
 
 function App() {
   return (
     <>
+      <Nav>
+        <Logo to={"/"}>
+          <GiKnifeFork />
+          Deliciouss...
+        </Logo>
+      </Nav>
       <Search />
       <Category />
       <Pages />
@@ -14,4 +21,20 @@ function App() {
   );
 }
 
+const Logo = styled(Link)`
+  text-decoration: none;
+  font-size: 1.5rem;
+  font-weight: 400;
+  font-family: "Lobster Two", cursive;
+`;
+
+const Nav = styled.div`
+  padding: 4rem 0rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  svg {
+    font-size: 2rem;
+  }
+`;
 export default App;
