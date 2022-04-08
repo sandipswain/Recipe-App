83d80cdb86836cd81a59da6e17af12cf1bd0360e
Sandip Swain <swainsandip9@gmail.com>
2022-04-08 21:16:10 +0530

    Added animations using Framer Motion

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -19,7 +19,12 @@ const Cuisine = () => {
     getCuisine(params.type);
   }, [params.type]);
   return (
-    <Grid>
+    <Grid
+      animate={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}
+    >
       {cuisine.map((item) => {
         return (
           <Card key={item.id}>
@@ -34,7 +39,7 @@ const Cuisine = () => {
   );
 };
 
-const Grid = styled.div`
+const Grid = styled(motion.div)`
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
   grid-gap: 3rem;
